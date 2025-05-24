802eafe087c8a0a099df125d90d7fbd779634812
Javier Lima <javi.limashelby@gmail.com>
2025-05-24 09:28:18 -0400

    refactor: Refactor benchmark
    - Use /usr/bin/time to gather measurements. It's a more accurate approach

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -1,73 +1,77 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
-	"runtime"
-	"time"
+	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/limaJavier/timetabling/pkg/model"
-	"github.com/limaJavier/timetabling/pkg/sat"
 
 	"github.com/samber/lo"
 )
 
-const satisfiableTestDirectory = "../../test/out/satisfiable/"
-const unsatisfiableTestDirectory = "../../test/out/unsatisfiable/"
-const KB = 1024
-const MB float32 = 1024 * 1024
+const (
+	executablePath                     = "../../bin/timetable"
+	satisfiableTestDirectory           = "../../test/out/satisfiable/"
+	unsatisfiableTestDirectory         = "../../test/out/unsatisfiable/"
+	KB                                 = 1024
+	MB                         float32 = 1024 * 1024
+)
 
 type TimetablerType int
 
 const (
-	Embedded TimetablerType = iota
-	Isolated
-	HybridIsolated
+	pure TimetablerType = iota
+	postponed
+	hybrid
 )
 
 type SolverType int
 
 const (
-	Kissat SolverType = iota
-	Cadical
-	Minisat
-	Cryptominisat
-	Slime
-	Ortoolsat
-	GlucoseSimp
-	GlucoseSyrup
+	kissat SolverType = iota
+	cadical
+	minisat
+	cryptominisat
+	slime
+	ortoolsat
+	glucosesimp
+	glucosesyrup
 )
 
 type ResultType int
 
 const (
-	Solved ResultType = iota
-	Unsatisfiable
-	Timeout
+	solved ResultType = iota
+	unsatisfiable
+	timeout
 )
 
 var (
 	timetablerTypes = map[TimetablerType]string{
-		Embedded:       "Embedded",
-		Isolated:       "Isolated",
-		HybridIsolated: "Hybrid Isolated",
+		pure:      "pure",
+		postponed: "postponed",
+		hybrid:    "hybrid",
 	}
 	solverTypes = map[SolverType]string{
-		Kissat:        "Kissat",
-		Cadical:       "Cadical",
-		Minisat:       "Minisat",
-		Cryptominisat: "Cryptominisat",
-		Slime:         "Slime",
-		Ortoolsat:     "Ortoolsat",
-		GlucoseSimp:   "GlucoseSimp",
-		GlucoseSyrup:  "GlucoseSyrup",
+		kissat:        "kissat",
+		cadical:       "cadical",
+		minisat:       "minisat",
+		cryptominisat: "cryptominisat",
+		slime:         "slime",
+		ortoolsat:     "ortoolsat",
+		glucosesimp:   "glucosesimp",
+		glucosesyrup:  "glucosesyrup",
 	}
 	resultTypes = map[ResultType]string{
-		Solved:        "Solved",
-		Unsatisfiable: "Unsatisfiable",
-		Timeout:       "Timeout",
+		solved:        "solved",
+		unsatisfiable: "unsatisfiable",
+		timeout:       "timeout",
 	}
 )
 
@@ -87,28 +91,13 @@ type TimetablerMetadata struct {
 }
 
 type BenchmarkResult struct {
-	Solver     SolverType
-	Timetabler TimetablerMetadata
-	Test       TestMetadata
-	Duration   int64
-	Memory     float32
-	Result     ResultType
-}
-
-type timetablerMetadata struct {
-	TimetablerMetadata
-	Constructor func(solver sat.SATSolver) model.Timetabler
-}
-
-type solverMetadata struct {
-	Type        SolverType
-	Constructor func() sat.SATSolver
-}
-
-type testMetadata struct {
-	Name        string
-	Satisfiable bool
-	Input       model.ModelInput
+	Solver        SolverType
+	Timetabler    TimetablerMetadata
+	Test          TestMetadata
+	Duration      int64
+	Memory        float32
+	CpuPercentage int64
+	Result        ResultType
 }
 
 func main() {
@@ -117,41 +106,23 @@ func main() {
 	solvers := getSolvers()
 	results := make([]BenchmarkResult, 0, len(tests)*len(timetablers)*len(solvers))
 
-	var testName, solverName, timetablerName string
+	tests = tests[:1]
 
-	defer func() {
-		if panic := recover(); panic != nil {
-			fmt.Printf("a panic occurred with test \"%v\", solver \"%v\" and timetabler \"%v\": %v\n", testName, solverName, timetablerName, panic)
-		}
-	}()
 	for _, test := range tests {
-		testName = test.Name
-		input := test.Input
+		for _, timetabler := range timetablers {
+			for _, solver := range solvers {
+				fmt.Printf("Benchmarking test \"%v\" with strategy \"%v\", solver \"%v\" and similarity \"%v\"\n", test.Name, timetablerTypes[timetabler.Type], solverTypes[solver], timetabler.RoomSimilarityThreshold)
 
-		for _, solverMeta := range solvers {
-			solverName = solverTypes[solverMeta.Type]
-			solver := solverMeta.Constructor()
-			for _, timetablerMeta := range timetablers {
-				timetablerName = timetablerTypes[timetablerMeta.Type]
-				timetabler := timetablerMeta.Constructor(solver)
-
-				duration, memory, result := measure(timetabler, input)
+				duration, maxMemory, cpuPercentage, result := measure(timetabler.Type, solver, timetabler.RoomSimilarityThreshold, test.Name)
 
 				results = append(results, BenchmarkResult{
-					Solver:     solverMeta.Type,
-					Timetabler: timetablerMeta.TimetablerMetadata,
-					Test: TestMetadata{
-						Name:              test.Name,
-						Satisfiable:       test.Satisfiable,
-						Subjects:          len(input.Subjects),
-						Professors:        len(input.Professors),
-						SubjectProfessors: len(input.SubjectProfessors),
-						Rooms:             len(input.Rooms),
-						Classes:           len(input.Classes),
-					},
-					Duration: duration,
-					Memory:   memory,
-					Result:   result,
+					Solver:        solver,
+					Timetabler:    timetabler,
+					Test:          test,
+					Duration:      duration,
+					Memory:        maxMemory,
+					CpuPercentage: cpuPercentage,
+					Result:        result,
 				})
 			}
 		}
@@ -160,8 +131,8 @@ func main() {
 	toCsv(results)
 }
 
-func getTests() []testMetadata {
-	tests := make([]testMetadata, 0)
+func getTests() []TestMetadata {
+	tests := make([]TestMetadata, 0)
 	for _, tuple := range lo.Zip2([]string{satisfiableTestDirectory, unsatisfiableTestDirectory}, []bool{true, false}) {
 		directory, satisfiable := tuple.A, tuple.B
 		testFiles, err := os.ReadDir(directory)
@@ -173,17 +144,17 @@ func getTests() []testMetadata {
 			filename := directory + file.Name()
 			input, err := model.InputFromJson(filename)
 			if err != nil {
-				panic("cannot parse input file: " + err.Error())
+				log.Fatalf("cannot parse input file: %v", err)
 			}
 
-			name := file.Name()
-			if !satisfiable {
-				name = "unsatisfiable_" + name
-			}
-			tests = append(tests, testMetadata{
-				Name:        name,
-				Satisfiable: satisfiable,
-				Input:       input,
+			tests = append(tests, TestMetadata{
+				Name:              filename,
+				Satisfiable:       satisfiable,
+				Subjects:          len(input.Subjects),
+				Professors:        len(input.Professors),
+				SubjectProfessors: len(input.SubjectProfessors),
+				Rooms:             len(input.Rooms),
+				Classes:           len(input.Classes),
 			})
 		}
 	}
@@ -191,114 +162,69 @@ func getTests() []testMetadata {
 	return tests
 }
 
-func getSolvers() []solverMetadata {
-	return []solverMetadata{
+func getSolvers() []SolverType {
+	return []SolverType{kissat, cadical, minisat, cryptominisat, slime, ortoolsat, glucosesimp, glucosesyrup}
+}
+
+func getTimetablers() []TimetablerMetadata {
+	return []TimetablerMetadata{
 		{
-			Type:        Kissat,
-			Constructor: sat.NewKissatSolver,
+			Type: pure,
 		},
+
 		{
-			Type:        Cadical,
-			Constructor: sat.NewCadicalSolver,
+			Type: postponed,
 		},
+
 		{
-			Type:        Minisat,
-			Constructor: sat.NewMinisatSolver,
+			Type:                    hybrid,
+			RoomSimilarityThreshold: 0.35,
 		},
+
 		{
-			Type:        Cryptominisat,
-			Constructor: sat.NewCryptominisatSolver,
+			Type:                    hybrid,
+			RoomSimilarityThreshold: 0.5,
 		},
+
 		{
-			Type:        Slime,
-			Constructor: sat.NewSlimeSolver,
-		},
-		{
-			Type:        Ortoolsat,
-			Constructor: sat.NewOrtoolsatSolver,
-		},
-		{
-			Type:        GlucoseSimp,
-			Constructor: sat.NewGlucoseSimpSolver,
-		},
-		{
-			Type:        GlucoseSyrup,
-			Constructor: sat.NewGlucoseSyrupSolver,
+			Type:                    hybrid,
+			RoomSimilarityThreshold: 0.75,
 		},
 	}
 }
 
-func getTimetablers() []timetablerMetadata {
-	return []timetablerMetadata{
-		{
-			TimetablerMetadata: TimetablerMetadata{
-				Type: Embedded,
-			},
-			Constructor: model.NewEmbeddedRoomTimetabler,
-		},
+func measure(timetable TimetablerType, solver SolverType, roomSimilarity float32, testFile string) (duration int64, maxMemory float32, cpuPercentage int64, result ResultType) {
+	cmd := exec.Command("/usr/bin/time", "-v", executablePath, "-strategy", timetablerTypes[timetable], "-solver", solverTypes[solver], "-similarity", fmt.Sprint(roomSimilarity), "-file", testFile)
 
-		{
-			TimetablerMetadata: TimetablerMetadata{
-				Type: Isolated,
-			},
-			Constructor: func(solver sat.SATSolver) model.Timetabler {
-				return model.NewIsolatedRoomTimetabler(solver, false, 0)
-			},
-		},
+	var stdOut bytes.Buffer
+	cmd.Stdout = &stdOut
+	var stdErr bytes.Buffer
+	cmd.Stderr = &stdErr
 
-		{
-			TimetablerMetadata: TimetablerMetadata{
-				Type:                    HybridIsolated,
-				RoomSimilarityThreshold: 0.35,
-			},
-			Constructor: func(solver sat.SATSolver) model.Timetabler {
-				return model.NewIsolatedRoomTimetabler(solver, true, 0.35)
-			},
-		},
-
-		{
-			TimetablerMetadata: TimetablerMetadata{
-				Type:                    HybridIsolated,
-				RoomSimilarityThreshold: 0.5,
-			},
-			Constructor: func(solver sat.SATSolver) model.Timetabler {
-				return model.NewIsolatedRoomTimetabler(solver, true, 0.5)
-			},
-		},
-
-		{
-			TimetablerMetadata: TimetablerMetadata{
-				Type:                    HybridIsolated,
-				RoomSimilarityThreshold: 0.75,
-			},
-			Constructor: func(solver sat.SATSolver) model.Timetabler {
-				return model.NewIsolatedRoomTimetabler(solver, true, 0.75)
-			},
-		},
+	cmd.Run()
+	if cmd.ProcessState.ExitCode() != 10 && cmd.ProcessState.ExitCode() != 20 {
+		log.Fatalf("an error occurred during the execution \"timetable\" at test \"%v\" using strategy \"%v\", solver \"%v\", room-similarity \"%v\": %v\n", testFile, timetablerTypes[timetable], solverTypes[solver], roomSimilarity, stdErr.String())
+	} else if cmd.ProcessState.ExitCode() == 20 {
+		result = unsatisfiable
+	} else {
+		result = solved
 	}
-}
-
-func measure(timetabler model.Timetabler, input model.ModelInput) (duration int64, memory float32, result ResultType) {
-
-	var memoryStart, memoryEnd runtime.MemStats
-	runtime.GC() // Clean up before measuring
-	runtime.ReadMemStats(&memoryStart)
-
-	start := time.Now()
-	timetable, err := timetabler.Build(input)
-	duration = time.Since(start).Milliseconds()
-	runtime.ReadMemStats(&memoryEnd)
-	memory = float32(memoryEnd.Alloc-memoryStart.Alloc) / MB
-	if err != nil {
-		panic("an error occurred during timetable-building: " + err.Error())
-	} else if timetable == nil {
-		return duration, memory, Unsatisfiable
+	splits := strings.Split(stdErr.String(), "\n")
+	getLine := func(substr string) string {
+		line, ok := lo.Find(splits, func(line string) bool {
+			return strings.Contains(strings.ToLower(line), substr)
+		})
+		if !ok {
+			log.Fatalf("Substring \"%v\" could not be found", substr)
+		}
+		return line
 	}
 
-	if !timetabler.Verify(timetable, input) {
-		panic("timetable verification failed")
-	}
-	return duration, memory, Solved
+	duration = parseDurationLine(getLine("wall clock"))
+	maxMemory = parseMemoryLine(getLine("maximum resident set size"))
+	cpuPercentage = parseCpuPercentageLine(getLine("percent of cpu"))
+
+	return duration, maxMemory, cpuPercentage, result
 }
 
 func toCsv(results []BenchmarkResult) {
@@ -311,7 +237,7 @@ func toCsv(results []BenchmarkResult) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	header := []string{"Solver", "Timetabler", "Room-Similarity Threshold", "Test", "Satisfiable", "Subjects", "Professors", "SubjectProfessors", "Rooms", "Classes", "Duration(ms)", "Memory(MB)", "Result"}
+	header := []string{"Solver", "Timetabler", "Room-Similarity Threshold", "Test", "Satisfiable", "Subjects", "Professors", "SubjectProfessors", "Rooms", "Classes", "Duration(ms)", "Memory(MB)", "CPU(%)", "Result"}
 	if err := writer.Write(header); err != nil {
 		log.Panicf("cannot write CSV header: %v", err)
 	}
@@ -330,10 +256,50 @@ func toCsv(results []BenchmarkResult) {
 			fmt.Sprintf("%d", result.Test.Classes),
 			fmt.Sprintf("%d", result.Duration),
 			fmt.Sprintf("%.1f", result.Memory),
+			fmt.Sprintf("%d", result.CpuPercentage),
 			resultTypes[result.Result],
 		}
 		if err := writer.Write(record); err != nil {
 			log.Panicf("cannot write CSV record: %v", err)
 		}
 	}
+}
+
+func parseDurationLine(line string) int64 {
+	durationStr := strings.Split(line, "(h:mm:ss or m:ss):")[1][1:]
+	return parseDuration(durationStr)
+}
+
+func parseDuration(durationStr string) int64 {
+	parts := strings.Split(durationStr, ":")
+	secondsStr := parts[len(parts)-1]
+	secondsParts := strings.Split(secondsStr, ".")
+
+	var duration int64
+	if len(parts) == 3 { // h:mm:ss
+		hours := lo.Must(strconv.Atoi(parts[0]))
+		minutes := lo.Must(strconv.Atoi(parts[1]))
+		seconds := lo.Must(strconv.Atoi(secondsParts[0]))
+		hundredthOfSeconds := lo.Must(strconv.Atoi(secondsParts[1]))
+		duration = int64(hours*3600+minutes*60+seconds)*1000 + int64(hundredthOfSeconds*10)
+	} else if len(parts) == 2 { // m:ss
+		minutes := lo.Must(strconv.Atoi(parts[0]))
+		seconds := lo.Must(strconv.Atoi(secondsParts[0]))
+		hundredthOfSeconds := lo.Must(strconv.Atoi(secondsParts[1]))
+		duration = int64(minutes*60+seconds)*1000 + int64(hundredthOfSeconds*10)
+	} else {
+		log.Fatalf("unexpected duration format: %v", durationStr)
+	}
+	return duration
+}
+
+func parseMemoryLine(line string) float32 {
+	memoryStr := strings.Split(line, ":")[1][1:]
+	return float32(lo.Must(strconv.ParseFloat(memoryStr, 32))) / 1024
+}
+
+func parseCpuPercentageLine(line string) int64 {
+	percentageStr := strings.Split(line, ":")[1][1:]
+	percentageStr = percentageStr[:len(percentageStr)-1]
+	return int64(lo.Must(strconv.Atoi(percentageStr)))
 }
